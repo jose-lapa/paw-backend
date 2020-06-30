68bf275919556c95950653a89d24fa1e4a1976ef
José Lapa <joselapa@Joses-MacBook-Pro.local>
2020-06-30 19:33:58 +0100

    Updated product router

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -4,4 +4,16 @@ const router = express.Router();
 const AuthRouter = require('./authRoute');
 const ProductController = require('../controllers/productControllers');
 
+router.get( '/', ( req, res, next ) => ProductController.show );
+
+router.post( '/', ( req, res, next ) => ProductController.createProduct );
+
+router.get( '/:name', ( req, res, next ) => ProductController.getByName );
+
+router.get( '/:id', ( req, res, next ) => ProductController.getById );
+
+router.put( '/:id', ( req, res, next ) => ProductController.updateById );
+
+router.delete( '/:id', ( req, res, next ) => ProductController.deleteById );
+
 module.exports = router;
