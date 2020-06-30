64419a5c3b3516ad286eb35493b0893d01e2c82c
José Lapa <joselapa@Joses-MacBook-Pro.local>
2020-06-30 11:41:37 +0100

    Updated main Router

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,19 @@ const express = require('express');
 var router = express.Router();
 
 const AuthRouter = require('./authRoute');
-const UserRouter = require('./UserRoute');
+const AccountRouter = require('./accountRoute');
+const ProductRouter = require('./productRoute');
+const OrderRouter = require('./orderRoute');
+const ComplaintRouter = require('./complaintRoute');
 
 router.get('/', (_, res) => {
 	res.sendStatus(200);
 });
 
 router.use('/auth', AuthRouter);
-router.use('/user', UserRouter);
+router.use('/accounts', AccountRouter);
+router.use('/products', ProductRouter);
+router.use('/order', OrderRouter);
+router.use('/complaint', ComplaintRouter);
 
 module.exports = router;
